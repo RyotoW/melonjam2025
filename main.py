a3c246e440d4dc9378445bf3a5a072863c94e8b6
arinj4 <arinjayj4@gmail.com>
2025-12-27 21:37:58 -0800

    made player class

--- main.py
+++ main.py
@@ -3,4 +3,13 @@
 import pygame
 import sys
 
+class Player(pygame.sprite.Sprite):
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+#    self.image = 
+    self.rect = self.image.get_rect()
+  def move(self, distance_x, distance_y)
+    self.rect.move_ip(distance_x, distance_y)
+
+
 pygame.init()
